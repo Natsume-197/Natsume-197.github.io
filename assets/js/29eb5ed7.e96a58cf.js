9834bbb945c097811acef23762a7487e416a6a62
Xerge-kai <disexile@protonmail.com>
2022-07-08 13:32:04 -0500

    Deploy website - based on 6b881a0f3e19c1de097f485a7d3a6b473492feed

---
+++ assets/js/29eb5ed7.e96a58cf.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbrigada_sos=self.webpackChunkbrigada_sos||[]).push([[300],{7085:s=>{s.exports=JSON.parse('{"name":"docusaurus-theme-search-algolia","id":"default"}')}}]);
